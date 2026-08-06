ae80fa0aebdb3410f488a99504c7fa8de2651f71
Carolina Morales <carolmor@cisco.com>
2026-08-06 15:18:06 -0600

    Remove temporary start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import { useState } from 'react';
-import HospitalityLanding from './pages/HospitalityLanding';
 import HospitalityExperience from './pages/HospitalityExperience';
 
 function App() {
-  const [experienceStarted, setExperienceStarted] = useState(false);
-
-  if (experienceStarted) {
-    return <HospitalityExperience />;
-  }
-
-  return (
-    <div>
-      <HospitalityLanding />
-
-      <button
-        type="button"
-        onClick={() => setExperienceStarted(true)}
-        style={{
-          position: 'fixed',
-          right: '24px',
-          bottom: '24px',
-          padding: '12px 18px',
-        }}
-      >
-        Start test
-      </button>
-    </div>
-  );
+  return <HospitalityExperience />;
 }
 
 export default App;
